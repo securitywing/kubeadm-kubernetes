57bba0190e59b22256c6640179a37e450e414eb7
syras <syrasmamun@gmail.com>
2020-03-10 15:28:59 +0000

    kops update

--- kops/kops.sh
+++ kops/kops.sh
@@ -12,6 +12,6 @@ sudo mv ./kubectl /usr/local/bin/kubectl
 # Create a cluster in AWS using KOPS,specifying the number of master nodes
 # Craete a s3 bucket to save the state
 
-kops create cluster --name=kube.yourdomain.com --state=s3://your-s3-buckt-name --zones=eu-west-1a --node-count=2 --node-size=t3.micro --master-size=t3.micro --dns-zone=kube.yourdomain.com
+kops create cluster --name=kube.yourdomain.com --state=s3://your-s3-bucket-name --zones=eu-west-1a --node-count=2 --node-size=t3.micro --master-size=t3.micro --dns-zone=kube.yourdomain.com
 
 
